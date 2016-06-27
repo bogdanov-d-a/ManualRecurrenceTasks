3aac958e98524d4da08632ae6b00a5b3d4b86f44
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2016-06-27 23:38:46 +0300

    Action buttons are now always visible

--- app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/MainActivity.java
+++ app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/MainActivity.java
@@ -25,7 +25,6 @@ import database.DatabaseHelper;
 // TODO: use black text everywhere
 // TODO: fix borders for activities and list items
 // TODO: place date and time buttons on the same row
-// TODO: action buttons should be always visible
 // TODO: add cancel button
 
 public class MainActivity extends AppCompatActivity {
