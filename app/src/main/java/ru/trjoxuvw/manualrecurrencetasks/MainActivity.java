589e128878f81dad9fede7c187cd113525241b3f
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2016-06-27 23:50:00 +0300

    Place date and time buttons on the same row

--- app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/MainActivity.java
+++ app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/MainActivity.java
@@ -24,7 +24,6 @@ import database.DatabaseHelper;
 // TODO: update view on every show
 // TODO: use black text everywhere
 // TODO: fix borders for activities and list items
-// TODO: place date and time buttons on the same row
 
 public class MainActivity extends AppCompatActivity {
     public static final int ADD_RECORD_REQUEST = 0;
