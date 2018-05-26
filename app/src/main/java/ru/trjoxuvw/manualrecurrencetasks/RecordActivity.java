53b1181616f7c2c7fca706555949e61eae30042a
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-05-27 01:41:39 +0300

    Remove unused import

--- app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/RecordActivity.java
+++ app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/RecordActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
