19a35dd559c15c27c011cfa85f581e127a44d499
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2019-07-21 21:39:02 +0300

    Update project

--- app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/MainActivity.java
+++ app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.trjoxuvw.manualrecurrencetasks;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
