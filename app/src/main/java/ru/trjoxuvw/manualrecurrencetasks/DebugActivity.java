65db01b2b72627f2f1d50bbe011d258e055d7b5b
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2022-10-11 00:08:00 +0600

    Add success/error toasts

--- app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/DebugActivity.java
+++ app/src/main/java/ru/trjoxuvw/manualrecurrencetasks/DebugActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import database.DatabaseHelper;
 import notification.NotificationUtils;
@@ -55,7 +56,9 @@ public class DebugActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     Utils.copyFile(dbPath, getBackupPath());
+                    Toast.makeText(DebugActivity.this, "Success", Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
+                    Toast.makeText(DebugActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -67,7 +70,9 @@ public class DebugActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     Utils.copyFile(getBackupPath(), dbPath);
+                    Toast.makeText(DebugActivity.this, "Success", Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
+                    Toast.makeText(DebugActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
